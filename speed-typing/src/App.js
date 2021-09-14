0f8a0b0a48e20c9d2c63db9b1d5f7296425c6df9
davidwpacker <davidwgpacker@gmail.com>
2021-09-14 12:55:43 +1000

    text area disabled when time runs out and changes color to grey

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -4,17 +4,14 @@ import './App.css'
 
 import React, {useState, useEffect} from "react"
 
-/**
- * Challenge:
- * 
- * Make it so clicking the Start button starts the timer instead of it starting on refresh
- * (Hint: use a new state variable to indicate if the game should be running or not)
- */
 
 function App() {
+    const STARTING_TIME = 5
+    
     const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(2)
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
     
     function handleChange(e) {
         const {value} = e.target
@@ -26,13 +23,25 @@ function App() {
         return wordsArr.filter(word => word !== "").length
     }
     
+    function startGame() {
+        setIsTimeRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setText("")
+    }
+    
+    function endGame() {
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+    }
+    
+    
     useEffect(() => {
         if(isTimeRunning && timeRemaining > 0) {
             setTimeout(() => {
                 setTimeRemaining(time => time - 1)
             }, 1000)
-        } else if(timeRemaining === 0) { 
-            setIsTimeRunning(false)
+        } else if(timeRemaining === 0) {
+            endGame()
         }
     }, [timeRemaining, isTimeRunning])
     
@@ -42,15 +51,22 @@ function App() {
             <textarea
                 onChange={handleChange}
                 value={text}
+                disabled={!isTimeRunning}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => setIsTimeRunning(true)}>Start</button>
-            <h1>Word count: ???</h1>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
         </div>
     )
 }
 
 export default App
+
 
 
 
