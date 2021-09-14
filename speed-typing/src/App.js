45d65c453217032bfa46296a6b314d755d2a76bc
davidwpacker <davidwgpacker@gmail.com>
2021-09-14 10:38:36 +1000

    tracked the state (using hooks - useState() ) of the text in the textarea on every keystroke

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -2,20 +2,33 @@
 // import './App.css';
 import './App.css'
 
-import React from "react"
+import React, {useState} from "react"
 
 function App() {
+    const [text, setText] = useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    // console.log(text)
+    
     return (
         <div>
-            <h1> Speed Typer </h1>
-            <textarea /> 
-            
-            <h4> Time remaining: {}</h4>  
-            <button> START GAME </button>  
-            <h1> Word count: </h1>   
+            <h1>How fast do you type?</h1>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: ???</h4>
+            <button>Start</button>
+            <h1>Word count: ???</h1>
         </div>
     )
 }
 
 export default App
+
+
 
