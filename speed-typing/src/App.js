60ad94155e77f0db8812a4e110a2d0ad16e0d4ad
davidwpacker <davidwgpacker@gmail.com>
2021-09-14 12:23:58 +1000

    Made it so clicking the Start button starts the timer instead of it starting on refresh

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -7,23 +7,14 @@ import React, {useState, useEffect} from "react"
 /**
  * Challenge:
  * 
- * 1. Create state to hold the current value of the countdown timer.
- *    Display this time in the "Time Remaining" header
- * 
- * 2. Set up an effect that runs every time the `timeRemaining` changes
- *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
- * 
- *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
- *    a lot of extra work.
- * 
- *    Warning: there will be a bug in this, but we'll tackle that next
- * 
- * 3. Make it so the effect won't run if the time is already at 0
+ * Make it so clicking the Start button starts the timer instead of it starting on refresh
+ * (Hint: use a new state variable to indicate if the game should be running or not)
  */
 
 function App() {
     const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(5) // starting from 5s then counts down. 
+    const [timeRemaining, setTimeRemaining] = useState(2)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
     
     function handleChange(e) {
         const {value} = e.target
@@ -36,12 +27,14 @@ function App() {
     }
     
     useEffect(() => {
-        if(timeRemaining > 0) {
+        if(isTimeRunning && timeRemaining > 0) {
             setTimeout(() => {
-                setTimeRemaining(time => time - 1) // time here can be called anything since it's a param
+                setTimeRemaining(time => time - 1)
             }, 1000)
+        } else if(timeRemaining === 0) { 
+            setIsTimeRunning(false)
         }
-    }, [timeRemaining])
+    }, [timeRemaining, isTimeRunning])
     
     return (
         <div>
@@ -51,13 +44,14 @@ function App() {
                 value={text}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
+            <button onClick={() => setIsTimeRunning(true)}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
 }
 
 export default App
+
 
 
 
