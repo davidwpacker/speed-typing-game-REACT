23546e9a80426265595348ee7bca2976341c5e16
davidwpacker <davidwgpacker@gmail.com>
2021-09-14 10:16:19 +1000

    css/basic UI

---
+++ speed-typing/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+// import './App.css';
+import './App.css'
+
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <h1> Speed Typer </h1>
+            <textarea /> 
+            
+            <h4> Time remaining: {}</h4>  
+            <button> START GAME </button>  
+            <h1> Word count: </h1>   
+        </div>
+    )
+}
+
+export default App
+
